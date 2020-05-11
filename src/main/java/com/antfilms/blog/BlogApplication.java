d1625ef95e20a422a8bb29dd9972d44b6085353f
fangloved <1031506997@qq.com>
2020-05-11 13:14:16 +0800

    second

--- src/main/java/com/antfilms/blog/BlogApplication.java
+++ src/main/java/com/antfilms/blog/BlogApplication.java
@@ -5,13 +5,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 @MapperScan("com.antfilms.blog.mapper")
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogApplication.class);
+    }
 }
